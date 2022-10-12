f555c1cabe144949bc304748e584c33e09910e73
Jun Young <50202150+wsdx123@users.noreply.github.com>
2022-10-12 20:26:13 +0900

    feat: Output Management

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 - 디렉토리 생성, 구조 변경, webpack설치 등 기본 셋업
 - 설정파일 생성 및 적용, scripts에 단축 명령어 설정
 - 에셋처리(css, image, font 적용)
+
+### 20221012
+
+- 에셋처리(data파일(csv,xml) 적용)
+- 엔트리포인트 추가
+- HtmlWebpackPlugin 사용하여 index.html에 모든 번들을 자동으로 추가해줌
+- output.clean옵션으로 npm run build시에 /dist폴더를 정리하고 빌드
--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-import Data from "./data.xml";
-import Notes from "./data.csv";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
+  element.appendChild(btn);
 
   return element;
 }
